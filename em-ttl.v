864046a8eb6cdb8961b80761a8d7da29e6307024
Christopher Moran <chris.moran@emergent.com.au>
2021-10-26 09:30:34 +1100

    Adding 7420

--- em-ttl.v
+++ em-ttl.v
@@ -97,6 +97,24 @@ module em_7410(a1, b1, c1, y1, a2, b2, c2, y2, a3, b3, c3, y3) ;
 	not #(0:9:15, 0:10:15) (y3, a3 & b3 & c3) ;
 	
 endmodule
+
+// 7420
+module em_7420(a1, b1, c1, d1, y1, a2, b2, c2, d2, y2) ;
+	input a1 ;
+	input b1 ;
+	input c1 ;
+	input d1 ;
+	input a2 ;
+	input b2 ;
+	input c2 ;
+	input d2 ;
+	output y1 ;
+	output y2 ;
+	
+	not #(0:9:15, 0:10:15) (y1, a1 & b1 & c1 & d1) ;
+	not #(0:9:15, 0:10:15) (y2, a2 & b2 & c2 & d2) ;
+	
+endmodule
 	
 // 74138
 module em_74138(out, e1, e2, e3, a) ;
